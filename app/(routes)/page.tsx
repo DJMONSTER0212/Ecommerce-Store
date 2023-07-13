99e9db891f01a2763f5b4431d5b96339fa2031c1
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-07-13 21:11:06 +0530

    created product card and other components

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,24 @@
+import getBillboard from '@/actions/get-billboards';
+import getProducts from '@/actions/get-products';
+import Billboard from '@/components/Billboard'
+import ProductList from '@/components/product-list';
+import Container from '@/components/ui/container'
 import React from 'react'
 
-const HomePage = () => {
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({isFeatured:true});
+  const billboard = await getBillboard("8f94c0b2-20cf-46a4-a5d5-1c02c317f26b");
   return (
-    <div>
-      Hello world
-    </div>
+    <Container>
+      <div className='space-y-10 pb-10 '>
+        <Billboard data={billboard} />
+      </div>
+      <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 '>
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
   )
 }
 
