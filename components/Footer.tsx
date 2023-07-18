56b08b0f9acfda2f7728060f1778904a287628e6
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-07-18 22:43:52 +0530

    try again

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-white border-t'>
-      <div className='mx-auto py-10'>
-        <p className='text-center text-xs text-black'>
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
           2023 DJ's Store, Inc. All rights Reserved.
         </p>
       </div>
